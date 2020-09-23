7c3bb69a40c1a60db9618a91fdc5161d7c5fcb76
davidlin778 <davidlin2001@live.com>
2020-09-23 14:35:01 -0700

    Color changes and chicken added

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ var pole = document.getElementById("pole");
 var entry = document.getElementById("entry");
 var player = document.getElementById("player");
 var flying = 0;
+var score = 0;
+
 
 entry.addEventListener('animationiteration', () => {
     var any = -((Math.random()*300)+150);
     entry.style.top = any + "px";
+    score++;
 });
+
 
 setInterval(function(){
     var playerTop =
@@ -14,15 +18,28 @@ setInterval(function(){
     if(flying==0){
      player.style.top = (playerTop+3)+"px";
     }
+    var poleLeft = 
+    parseInt (window.getComputedStyle(pole).getPropertyValue("left"));
+    var entryTop = 
+    parseInt (window.getComputedStyle(entry).getPropertyValue("top"));
+    var pTop = -(500-playerTop);
+    if((playerTop>480)||((poleLeft<50)&&(poleLeft>-50)&&((pTop<entryTop)||(pTop>entryTop+130)))){
+        alert("Game over. Score: " + (score-1) );
+        player.style.top = 100 + "px";
+        score = 0;
+    
+    }
 },10);
 
+
+
 function fly(){
-    flying = 5;
+    flying = 1;
     let flyCount = 0;
     var flyInterval = setInterval(function(){
         var playerTop =
         parseInt (window.getComputedStyle(player).getPropertyValue("top"));
-        if(playerTop>6){
+        if((playerTop>6)&&(flyCount<15)){
             player.style.top = (playerTop-5)+"px";
         }
        
@@ -34,3 +51,4 @@ function fly(){
         flyCount++;
     },10);
 }
+
